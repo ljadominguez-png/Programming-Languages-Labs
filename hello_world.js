d76644713c2016013db6a61ef4d898e054899a5c
LaurenceDominguez <dominguezlaurencejhona@gmail.com>
2026-08-14 14:46:02 +0800

    jabai skrip

---
+++ hello_world.js
@@ -0,0 +1,18 @@
+//mga pinaglalaro ko pag bored ako
+
+console.log("Hello world");
+
+function add(a,b){
+    return a + b;
+}
+
+console.log(add(1,2));
+
+function incrementer (x){
+    return x * x;
+}
+
+for(let i = 0; i < 500000; i++){
+    console.log(incrementer(i));
+}
+
